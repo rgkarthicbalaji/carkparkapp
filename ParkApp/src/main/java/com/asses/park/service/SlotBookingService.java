94f50300a271df6febacb37b54bb6f6e1a9500e5
rgkarthicbalaji <karthicbalaji@gmail.com>
2021-07-12 18:25:22 +0530

    Modified fixed delay so that there is a 30 minutes grace period for a customer to reallocate. If the customer does not perform either reallocate or cancel within this grace period, auto reallocate happens with the same hourslot which he has early provided for previous booking.

--- ParkApp/src/main/java/com/asses/park/service/SlotBookingService.java
+++ ParkApp/src/main/java/com/asses/park/service/SlotBookingService.java
@@ -175,7 +175,7 @@ public class SlotBookingService {
         return slotUsageHours;
     }
 
-    @Scheduled(initialDelay = 60000, fixedRate = 60000)
+    @Scheduled(initialDelay = 60000, fixedRate = 1800000)
     @Transactional
     public void runParkingSlotReallocationTask() throws Exception {
         Optional<List<SlotBooking>> slotBookingsOptional = Optional.ofNullable(slotBookingRepository.findByIsBookedNow(Boolean.TRUE));
